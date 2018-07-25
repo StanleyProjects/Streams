7da80b5ba260e88a34054087db1378faf2d43cc1
kepocnhh <kepocnhh@gmail.com>
2018-07-25 16:20:27 +0300

    close #3

--- sample8/src/main/java/stan/streams/App.java
+++ sample8/src/main/java/stan/streams/App.java
@@ -1,88 +1,10 @@
 package stan.streams;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Collectors;
-
 public class App
 {
     static public void main(String[] args)
     {
         System.out.println("\tJava 8 Streams demonstration");
-        System.out.println("Basic functions:");
-        foreachSample(Arrays.asList(1, 2, 3, 4, 5));
-        mapSample(Arrays.asList("aa", "bbb", "cccc", "dd", "e"));
-        filterSample(Arrays.asList("Cat", "Dog", "Penguin", "Platypus", "Elephant", "Camel", "Goat", "Lion", "Turtle", "Crab"));
-    }
-
-    static private void foreachSample(Collection<?> source)
-    {
-        System.out.println("\t- java 8 foreach for: " + source);
-        long time = System.nanoTime();
-        foreachJava8Sample(source);
-        System.out.println("\ttime: " + (System.nanoTime() - time)/1000);
-        System.out.println("\t- streams foreach for: " + source);
-        time = System.nanoTime();
-        foreachStreamsSample(source);
-        System.out.println("\ttime: " + (System.nanoTime() - time)/1000);
-    }
-    static private void foreachJava8Sample(Collection<?> collection)
-    {
-        collection.forEach(it -> System.out.println("n: " + it));
-    }
-    static private void foreachStreamsSample(Collection<?> collection)
-    {
-        Streams.from(collection)
-               .foreach(it -> System.out.println("n: " + it));
-    }
-
-    static private void mapSample(Collection<String> source)
-    {
-        System.out.println("\t- java 8 map for: " + source);
-        long time = System.nanoTime();
-        mapJava8Sample(source);
-        System.out.println("\ttime: " + (System.nanoTime() - time)/1000);
-        System.out.println("\t- streams map for: " + source);
-        time = System.nanoTime();
-        mapStreamsSample(source);
-        System.out.println("\ttime: " + (System.nanoTime() - time)/1000);
-    }
-    static private void mapJava8Sample(Collection<String> source)
-    {
-        System.out.println(source.stream()
-                                 .map(String::length)
-                                 .collect(Collectors.toList()));
-    }
-    static private void mapStreamsSample(Collection<String> source)
-    {
-        System.out.println(Streams.from(source)
-                                  .map(String::length)
-                                  .list());
-    }
-
-    static private void filterSample(Collection<String> source)
-    {
-        System.out.println("\t- java 8 filter for: " + source);
-        long time = System.nanoTime();
-        filterJava8Sample(source);
-        System.out.println("\ttime: " + (System.nanoTime() - time)/1000);
-        System.out.println("\t- streams filter for: " + source);
-        time = System.nanoTime();
-        filterStreamsSample(source);
-        System.out.println("\ttime: " + (System.nanoTime() - time)/1000);
-    }
-    private static void filterJava8Sample(Collection<String> source)
-    {
-        System.out.println(source.stream()
-                                 .filter(it -> it.toLowerCase().startsWith("c")
-                                     || it.toLowerCase().startsWith("p"))
-                                 .collect(Collectors.toList()));
-    }
-    private static void filterStreamsSample(Collection<String> source)
-    {
-        System.out.println(Streams.from(source)
-                                  .filter(it -> it.toLowerCase().startsWith("c")
-                                      || it.toLowerCase().startsWith("p"))
-                                  .list());
+        ImplementBasicFunctions.sample();
     }
 }
